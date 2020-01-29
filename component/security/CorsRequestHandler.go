3aacd571e08fcc348ac0d2bf74c77175162f8940
ewill.leung@foxmail.com <will89liangwenwei>
2020-01-29 16:54:25 +0800

    init

--- component/security/CorsRequestHandler.go
+++ component/security/CorsRequestHandler.go
@@ -5,7 +5,6 @@ import (
 	"github.com/xhminc/xhm-framework/component/logger"
 	"github.com/xhminc/xhm-framework/config"
 	"go.uber.org/zap"
-	"net/http"
 	"strings"
 )
 
@@ -22,8 +21,9 @@ func CorsRequestHandler() gin.HandlerFunc {
 
 	return func(c *gin.Context) {
 
-		method := c.Request.Method
+		//method := c.Request.Method
 		c.Header("Access-Control-Allow-Origin", strings.Join(globalConfig.Application.Cors.Hosts, ","))
+		c.Header("Access-Control-Max-Age", "3600")
 		c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, OPTIONS")
 		c.Header("Access-Control-Allow-Credentials", "true")
 		c.Header("Access-Control-Allow-Headers", "Content-Type, Authorization, Token")
@@ -31,9 +31,9 @@ func CorsRequestHandler() gin.HandlerFunc {
 			"Content-Type, Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers")
 		c.Header("Content-Type", "application/json")
 
-		if method == "OPTIONS" {
-			c.AbortWithStatus(http.StatusNoContent)
-		}
+		//if method == "OPTIONS" {
+		//	c.AbortWithStatus(http.StatusNoContent)
+		//}
 
 		c.Next()
 	}
