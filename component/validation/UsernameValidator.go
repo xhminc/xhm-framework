fc50c9e8f55537ecdec3164c973d79868f909c4d
ewill.leung@foxmail.com <will89liangwenwei>
2020-02-02 21:30:53 +0800

    init

--- component/validation/UsernameValidator.go
+++ component/validation/UsernameValidator.go
@@ -3,19 +3,10 @@ package validation
 import (
 	"github.com/xhminc/xhm-framework/config"
 	"gopkg.in/go-playground/validator.v9"
-	"reflect"
 )
 
-func Username(
-	v *validator.Validate,
-	topStruct reflect.Value,
-	currentStructOrField reflect.Value,
-	field reflect.Value,
-	fieldType reflect.Type,
-	fieldKind reflect.Kind,
-	param string,
-) bool {
-	if username, ok := field.Interface().(string); ok {
+func Username(fieldLevel validator.FieldLevel) bool {
+	if username, ok := fieldLevel.Field().Interface().(string); ok {
 		return config.Mobile.MatchString(username) || config.Email.MatchString(username)
 	}
 	return false
