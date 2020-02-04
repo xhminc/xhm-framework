9946f9b0a153c867d965cefe1b31ef24dec11ef0
ewill.leung@foxmail.com <will89liangwenwei>
2020-02-04 18:21:22 +0800

    init

--- component/common/BaseFields.go
+++ component/common/BaseFields.go
@@ -3,6 +3,6 @@ package common
 import "time"
 
 type BaseFields struct {
-	UtcCreate   *time.Time `gorm:"utc_create;default:null" json:"utcCreate,omitempty"`
-	UtcModified *time.Time `gorm:"utc_modified;default:null" json:"utcModified,omitempty"`
+	UtcCreate   *time.Time `gorm:"utc_create;null" json:"utcCreate,omitempty"`
+	UtcModified *time.Time `gorm:"utc_modified;null" json:"utcModified,omitempty"`
 }
